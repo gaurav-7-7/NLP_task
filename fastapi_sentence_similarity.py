efed98b6e07efb817253f2bc8293c56e7e6d8df7
Gaurav Singh <52814356+gaurav-7-7@users.noreply.github.com>
2022-07-24 00:41:31 +0530

    Add files via upload

--- fastapi_sentence_similarity.py
+++ fastapi_sentence_similarity.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/login")
-async def login(username: str = Form(), password: str = Form()):
-    return {"username": username} 
+class s(BaseModel):
+    s1 : str
+    s2 : str
+
+from sentence_transformers import SentenceTransformer
+from sklearn.metric.pairwise import consine_similarity
+
+model = SentenceTransformer('bert-base-nli-mean-tokens')
+
+@app.post("/check_similarity")
+async def check_similarity(string : s):
+    return ({"score": str(consine_similarity([model.encode(string.s1), model.encode(string.s1)]))})
+    
+    
+"""
+  feeding the below json file to the api we get the similarity score
+ {
+  "s1": "Sentences are passed as a list of string.",
+  "s2": "The quick brown fox jumps over the lazy dog."
+ }
+"""
